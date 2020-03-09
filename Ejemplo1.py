5a0dd82789b994fc7b1026295cbaf12b65df47f7
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2020-03-09 12:33:54 +0100

    "ejemplo pruebas de software"

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,6 +1,8 @@
-
+#OJO CON LOS ESPACIOS !!!!
 def cadrados(lista):
-    # PONDREMOS EN LA DOCUMENTACIÓN CÓDIDO, QUE SERÁ EL QUE PROBAREMOS CON LA FUNCIÓN "_PROBA"
+    # PONDREMOS EN LA DOCUMENTACIÓN CÓDIDO, QUE SERÁ EL QUE PROBAREMOS CON LA FUNCIÓN "CADRADOS" Y "_PROBA"
+    #LO QUE ESTÁ ENTRE CORCHETES ES EL RESULTADO QUE ESPERAMOS [0,1,4,9,16]
+    #LO QUE PONEMOS EN >>> ES LA PRUEBA !!! Y LO QUE HAY AL FINAL DEL CÓDIGO DE >>> ES EL RESULTADO ESPERADO !!!
     """Calcula el cuadrado de los numeros de una lista
 
     >>> l = [0,1,2,3,4]
@@ -8,6 +10,24 @@ def cadrados(lista):
     [0, 1, 4, 9, 16]
     """
     return [n ** 2 for n in lista]
+
+
+#Creamos otra función
+def cadrados2(n):
+
+    """Calcula o cadrado do numero pasado como parametro
+
+    >>> l = [0,1,2,3,4]
+    >>> for n in l:
+    ... cadrad
+    4
+
+    :param n: numero
+    :return: cadrado do numero n**2
+    """
+
+    return n ** 2
+
 
 def _proba():
     import doctest
@@ -18,3 +38,4 @@ def _proba():
 if __name__ == "__main__":
     _proba()
     print(cadrados([0,1,2]))
+    print(cadrados2(2))
